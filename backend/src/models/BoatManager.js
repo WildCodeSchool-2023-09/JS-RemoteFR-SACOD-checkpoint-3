da173bc3808ac4c3f44e42c9fdbed2b73be8f036
francoiscrtz <francoiscreutzer@gmail.com>
2023-12-18 11:48:35 +0100

    test step2 ok

--- backend/src/models/BoatManager.js
+++ backend/src/models/BoatManager.js
@@ -6,11 +6,16 @@ class BoatManager extends AbstractManager {
   }
 
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await this.database.query(`select * from ${this.table}`);
-
-    // Return the array of boats
     return rows;
+  }
+
+  async update(id, x, y) {
+    const [result] = await this.database.query(
+      `UPDATE ${this.table} SET coord_x = ?, coord_y = ? where id = ?`,
+      [x, y, id]
+    );
+    return result;
   }
 }
 
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,22 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
 const boatControllers = require("./controllers/boatControllers");
 
 router.get("/boats", boatControllers.browse);
-
-const tileControllers = require("./controllers/tileControllers");
-
-router.get("/tiles", tileControllers.browse);
+router.put("/boats/:id", boatControllers.edit);
 
 const gameControllers = require("./controllers/gameControllers");
 
 router.post("/games", gameControllers.add);
 
 /* ************************************************************************* */
+
+const tileControllers = require("./controllers/tileControllers");
+
+router.get("/tiles", tileControllers.browse);
 
 module.exports = router;
