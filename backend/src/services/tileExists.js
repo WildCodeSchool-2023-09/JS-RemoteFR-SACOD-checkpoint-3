496d463c9bd1edc64a87900f0c1cbaf250f19245
Flavien G <44913995+fgaujard@users.noreply.github.com>
2023-12-18 12:55:12 +0100

    step 4 : start2

--- backend/src/services/tileExists.js
+++ backend/src/services/tileExists.js
@@ -1,6 +1,7 @@
 /* ************************************************************************* */
 // Step 4
 /* ************************************************************************* */
+// const tables = require("../tables");
 
 const middleware = (req, res, next) => {
   // 1 : SELECT coordX, coordY FROM tile WHERE id = ?
@@ -9,6 +10,14 @@ const middleware = (req, res, next) => {
   // eslint-disable-next-line radix
   const coordX = req.body.coord_x;
   const coordY = req.body.coord_y;
+  /*
+  try {
+    const tile = tables.tile.readByCoordinates();
+    res.json(tile);
+  } catch (err) {
+    next(err);
+  }
+  */
   if (coordX <= 11 && coordX >= 0 && coordY <= 5 && coordY >= 0) {
     next();
   } else {
