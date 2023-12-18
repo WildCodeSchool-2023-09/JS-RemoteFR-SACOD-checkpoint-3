ee81dbe0a5e39d3a015310f3f9564f90ed851f94
Ijuaa <pierre.colpart@gmail.com>
2023-12-18 10:59:51 +0100

    done update, easy

--- backend/src/router.js
+++ backend/src/router.js
@@ -9,11 +9,15 @@ const router = express.Router();
 const boatControllers = require("./controllers/boatControllers");
 
 router.get("/boats", boatControllers.browse);
+router.put("/boats/:id", boatControllers.edit);
 
 const gameControllers = require("./controllers/gameControllers");
 
 router.post("/games", gameControllers.add);
 
+const tileControllers = require("./controllers/tileControllers");
+
+router.get("/tiles", tileControllers.browse);
 /* ************************************************************************* */
 
 module.exports = router;
